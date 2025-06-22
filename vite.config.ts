7b2451fded316c5c3d385a9809ba5ca6de5b90d9
Harine Cellam <harinecellam2210730@ssn.edu.in>
2025-06-23 00:19:28 +0530

    commit index

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 });
